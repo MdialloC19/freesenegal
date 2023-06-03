b5ae25c779ba095c046da56df8b129a931354637
MdialloC19 <mdiallo3211@gmail.com>
2023-06-03 20:33:36 +0000

    new version

--- twitter.py
+++ twitter.py
@@ -1,19 +1,19 @@
 import tweepy
 import time
 
-consumer_key = "YOUR_CONSUMER_KEY"
-consumer_secret = "YOUR_CONSUMER_SECRET"
-access_token = "YOUR_ACCESS_TOKEN"
-access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+api_key = "rI7TjlWaAY5zzea3Q4lW2mvwu"
+api_secret = "GjB9sJUmdfKe8VWm79dpBGRFOD39jj9HSb8JJhAbTr26kkbnb7"
+access_token = "924722624390983680-Il0bifEUVC9BubiRbVA6F7DuepLxWDN"
+access_token_secret = "aqVFJRO047lFsn0t7ZdtEjrPOI3yLcG3wt49mbH13yS9o"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-tweet_id = "YOUR_TWEET_ID"
-custom_message = "Ceci est un retweet avec un message personnalisé !"
+tweet_id = "1665029495307350016?s=46&t=nDA5teviE0iP3mUjxm9VzA"
+custom_message = "@elonmusk!"
 
 while True:
     api.update_status(f"{custom_message} \n\n{tweet_id}")
     print("Le tweet a été retweeté avec succès.")
-    time.sleep(60)
+    time.sleep(10)
